2e666d6bb32e303d82cb1030a37a6d5dec968999
Jasper Yu <Jasperyu.kk@gmail.com>
2018-11-15 16:29:34 -0800

    add cost and new assignemnt #2

--- candybars.cpp
+++ candybars.cpp
@@ -2,30 +2,34 @@
 First assignment a user puts inputs an amount of candy bars. 
 Program calculates the total weight of bars and displays the number of bars and its weight.
 this ones simple
-Second assignment is pass two values to a function that calculates the sum of the two values.
-use two functions that passes both
-one is call by value
-other is call by reference
-after calculating the sum of the two values add that value to the first parameter
-and return the first parameter
 */
 
 #include <iostream>
 using namespace std;
 
-double calcWeight(int candybars) {
-	double weight = 3.33;
-	return weight * 5;
-}
-
 int main()
 {
-	int candyBars = 5;
-	std::cout << "Weight: ";
-	std::cout << calcWeight(candyBars);
-	std::cout << "oz" << std::endl;
-	std::cout << "# Candy Bars: ";
-	std::cout << candyBars << std::endl;
+	int candyBars;
+	float weight = 3.33;
+
+	cout << "Enter number of candybars: ";
+	cin >> candyBars;
+
+	//total weight
+	float totalWeight = 3.33 * candyBars;
+	cout << "Weight: ";
+	cout << totalWeight;
+	cout << "oz" << endl;
+
+	//total cost
+	int costPerOz = 2;
+	float cost = costPerOz * totalWeight;
+	cout << "Cost: $";
+	cout << cost << endl;
+
+	//number candy bars;
+	cout << "# Candy Bars: ";
+	cout << candyBars << endl;
 
 	return 0;
 }
---
+++ reference.cpp
@@ -0,0 +1,28 @@
+/*
+Second Assignment
+create two functions that both return sum of two values
+one of the function is call by value
+other is call by reference
+after calculating the sum of the two values, 
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumRef(int x, int y) 
+{
+	return x * y;
+};
+
+int sumVal(int x, int y) 
+{
+	return x * y;
+};
+
+int main()
+{
+	int x = 5;
+	int y = 9;
+	cout << "Weight: ";
+	return 0;
+}
